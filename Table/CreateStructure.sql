6df17affda58731f0125733ac22fe13a8f5d1d45
ilyateren <148451060+ilyateren@users.noreply.github.com>
2024-10-28 16:58:46 +0300

    Update CreateStructure.sql

--- Table/CreateStructure.sql
+++ Table/CreateStructure.sql
@@ -8,7 +8,7 @@ create table dbo.SKU (
 create table dbo.Family (
   ID int not null identity,
   SurName varchar(255) not null,
-  BudgetValue decimal(10, 2) not null,
+  BudgetValue decimal(18, 2) not null,
   constraint PK_Family primary key clustered (ID)
 );
 
@@ -17,9 +17,9 @@ create table dbo.Basket (
   ID_SKU int not null,
   ID_Family int not null,
   Quantity int not null check (Quantity >= 0),
-  Value decimal(10, 2) not null check (Value >= 0),
+  Value decimal(18, 2) not null check (Value >= 0),
   PurchaseDate date not null default getdate(),
-  DiscountValue decimal(10, 2) not null,
+  DiscountValue decimal(18, 2) not null,
   constraint PK_Basket primary key clustered (ID),
   constraint FK_Basket_ID_SKU_SKU foreign key (ID_SKU) references dbo.SKU(ID),
   constraint FK_Basket_ID_Family_Family foreign key (ID_Family) references dbo.Family(ID)
