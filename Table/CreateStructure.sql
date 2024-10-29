e905d467f992b96416da5a2e3109d26f814d802c
ilyateren <148451060+ilyateren@users.noreply.github.com>
2024-10-29 13:15:03 +0300

    Update CreateStructure.sql

--- Table/CreateStructure.sql
+++ Table/CreateStructure.sql
@@ -3,15 +3,13 @@ create table dbo.SKU (
   Code as ('s' + cast(ID as varchar)) not null unique,
   Name varchar(255) not null,
   constraint PK_SKU primary key clustered (ID)
-);
-
+)
 create table dbo.Family (
   ID int not null identity,
   SurName varchar(255) not null,
   BudgetValue decimal(18, 2) not null,
   constraint PK_Family primary key clustered (ID)
-);
-
+)
 create table dbo.Basket (
   ID int not null identity,
   ID_SKU int not null,
@@ -23,4 +21,4 @@ create table dbo.Basket (
   constraint PK_Basket primary key clustered (ID),
   constraint FK_Basket_ID_SKU_SKU foreign key (ID_SKU) references dbo.SKU(ID),
   constraint FK_Basket_ID_Family_Family foreign key (ID_Family) references dbo.Family(ID)
-);
+)
