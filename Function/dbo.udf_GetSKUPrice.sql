d01ee38ce68c4e2802ffc8639c1f12461ac5c72a
ilyateren <148451060+ilyateren@users.noreply.github.com>
2024-10-29 14:38:26 +0300

    Update dbo.udf_GetSKUPrice.sql

--- Function/dbo.udf_GetSKUPrice.sql
+++ Function/dbo.udf_GetSKUPrice.sql
@@ -1,24 +1,23 @@
-create or alter function dbo.udf_GetSKUPrice(
-	@ID_SKU int
-)
-returns decimal(18, 2)
+create procedure dbo.usp_MakeFamilyPurchase
+@FamilySurName varchar(255)
 as
 begin
-	declare
-		@Price decimal(18, 2)
-		,@TotalQuantity int
-		,@TotalValue decimal(18, 2)
-	
-	select
-		@TotalQuantity = sum(quantity)
-		,@TotalValue = sum(value)
-	from dbo.Basket as b
-	where b.ID_SKU = @ID_SKU
-
-	if (@TotalQuantity = 0)
-		set @Price = 0
-	else
-		set @Price = @TotalValue / @TotalQuantity
-	
-	return @Price
+	if not exists (
+		select f.SurName
+		from dbo.Family as f
+		where f.SurName = @FamilySurName
+	)
+	begin
+		print 'Данной семьи нет'
+		return;
+	end
+	update f
+	set f.BudgetValue = f.BudgetValue - (
+		select sum(b.Value)
+		from dbo.Family as f
+			inner join dbo.Basket as b on b.ID_Family = f.ID
+		where f.SurName = @FamilySurName
+  )
+	from dbo.Family as f
+	where f.SurName = @FamilySurName
 end;
